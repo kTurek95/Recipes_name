53ca2fe4a50538108f2dfffb144d42bd490a0f9e
kTurek95 <k.turek1995@gmail.com>
2023-06-10 15:14:39 +0100

    Added main function, moved everything to the module with functions

--- Recipes_refigerator2.py
+++ Recipes_refigerator2.py
@@ -1,15 +1,4 @@
-import function_recpies
+from function_recipes import main
 
-ingredients = []
-
-while True:
-    user_ingredients = input('Please provide the ingredients you have (or type "end" to finish): ')
-    if user_ingredients.lower() == 'end':
-        break
-    else:
-        ingredients.append(user_ingredients)
-
-
-function_recpies.get_ingredients_from_user_and_save_in_file(ingredients)
-function_recpies.get_recipes(ingredients)
-function_recpies.save_recipes_name_to_file(function_recpies.get_recipes(ingredients))
+if __name__ == '__main__':
+    main()
